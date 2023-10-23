a7340709398487584bfbcfecf5707c31f39ff117
Amir Mehrafsa <mexuaz@outlook.com>
2023-10-23 13:28:16 -0700

    Constraint running job to cascade

--- build-decompmain-gpu.sh
+++ build-decompmain-gpu.sh
@@ -5,11 +5,12 @@
 #SBATCH --output=build-arb-nucleus-decomp-gpu.out
 #SBATCH --error=build-arb-nucleus-decomp-gpu.err
 #SBATCH --nodes=1 # Number of nodes
-#SBATCH --gres=cpu:16 # Number of CPUs (shared or exclusive)
+#SBATCH --gres=cpu:32 # Number of CPUs (Mac cores on Cascade Lake with GPU)
 #SBATCH --gres=gpu:1 # Number of GPUs (per node)
-#SBATCH --mem=64G # Memory per node
+#SBATCH --mem=192000M # Memory per node (Max memory on Cascade Lake with GPU 187G)
 #SBATCH --ntasks-per-node=1 # Number of tasks per node
-#SBATCH --time=03:00:00 # Time limit hrs:min:sec
+#SBATCH --time=10:00:00 # Time limit hrs:min:sec
+#SBATCH --constraint=cascade # define specific Node
 
 
 #rm build-arb-nucleus-decomp-gpu.out build-arb-nucleus-decomp-gpu.err
